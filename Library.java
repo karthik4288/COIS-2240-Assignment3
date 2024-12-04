f6d4495557f9c846618578959c1efd306c1beb36
karthik4288 <karthikk180725@gmail.com>
2024-12-04 14:07:43 -0500

    The program now validates ID numbers for books and members
    Unlike previously, two users can not exist with the same username , neither can two books with the same book id.

---
+++ Library.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Member> members = new ArrayList<Member>();
+    private List<Book> books = new ArrayList<Book>();
+
+    // Add a new member to the library
+    public boolean addMember(Member member) {
+    	if(findMemberById(member.getId()) != null) {
+    		System.out.println("Error! ID no already being used");
+    		return false;
+    	}
+        members.add(member);
+        return true;
+    }
+    
+    // Add a new book to the library
+    public boolean addBook(Book book) {
+    	if(findBookById(book.getId()) != null) {
+    		System.out.println("Error! Book ID no already being used");
+    		return false;
+    	}
+        books.add(book);
+        return true;
+    }
+
+    // Find a member by ID
+    public Member findMemberById(int id) {
+        for (Member member : members) {
+            if (member.getId() == id) {
+                return member;
+            }
+        }
+        return null;
+    }
+
+    // Find a book by ID
+    public Book findBookById(int id) {
+        for (Book book : books) {
+            if (book.getId() == id) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    // Get the list of members
+    public List<Member> getMembers() {
+        return members;
+    }
+    
+    // Get the list of books
+    public List<Book> getBooks() {
+        return books;
+    }
+}
